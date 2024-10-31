77f7155ce9e3ef4cb8bb9959c5f54d60d005b18f
O1e2na <155379499+O1e2na@users.noreply.github.com>
2024-10-31 11:13:19 +0200

    commit mesage

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,19 +1,27 @@
 import axios from 'axios';
 
-// Використовуйте API_URL для базового URL
-const API_URL = 'https://api.themoviedb.org/3/';
-const API_TOKEN = process.env.REACT_APP_API_TOKEN;
+
+const API_TOKEN = import.meta.env.VITE_API_TOKEN;
 
 const instance = axios.create({
-  baseURL: API_URL,
+  baseURL: 'https://api.themoviedb.org/3/',  
   headers: {
-    Authorization: `Bearer ${API_TOKEN}`, // Додайте Bearer перед токеном
+    Authorization: `Bearer ${API_TOKEN}`,  
   },
 });
 
-// Функції для API запитів
-export const fetchTrendingMovies = () => instance.get('/trending/movie/day').then(res => res.data);
-export const fetchMoviesByQuery = (query) => instance.get('/search/movie', { params: { query } }).then(res => res.data);
-export const fetchMovieDetails = (movieId) => instance.get(`/movie/${movieId}`).then(res => res.data);
-export const fetchMovieCast = (movieId) => instance.get(`/movie/${movieId}/credits`).then(res => res.data);
-export const fetchMovieReviews = (movieId) => instance.get(`/movie/${movieId}/reviews`).then(res => res.data);
+
+export const fetchTrendingMovies = () => 
+  instance.get('/trending/movie/day').then(res => res.data);
+
+export const fetchMoviesByQuery = (query) => 
+  instance.get('/search/movie', { params: { query } }).then(res => res.data);
+
+export const fetchMovieDetails = (movieId) => 
+  instance.get(`/movie/${movieId}`).then(res => res.data);
+
+export const fetchMovieCast = (movieId) => 
+  instance.get(`/movie/${movieId}/credits`).then(res => res.data);
+
+export const fetchMovieReviews = (movieId) => 
+  instance.get(`/movie/${movieId}/reviews`).then(res => res.data);
